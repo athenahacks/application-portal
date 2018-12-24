156a4e605eca0a723bcf0bb523acf15aefd61d1a
Sophie Wu <kuanyiwu@s0ph1e-2.local>
2018-12-24 14:49:30 +0800

    initialize

---
+++ app/client/views/admin/users/adminUsersCtrl.js
@@ -0,0 +1,316 @@
+angular.module('reg')
+  .controller('AdminUsersCtrl',[
+    '$scope',
+    '$state',
+    '$stateParams',
+    'UserService',
+    function($scope, $state, $stateParams, UserService){
+
+      $scope.pages = [];
+      $scope.users = [];
+
+      // Semantic-UI moves modal content into a dimmer at the top level.
+      // While this is usually nice, it means that with our routing will generate
+      // multiple modals if you change state. Kill the top level dimmer node on initial load
+      // to prevent this.
+      $('.ui.dimmer').remove();
+      // Populate the size of the modal for when it appears, with an arbitrary user.
+      $scope.selectedUser = {};
+      $scope.selectedUser.sections = generateSections({status: '', confirmation: {
+        dietaryRestrictions: []
+      }, profile: ''});
+
+      function updatePage(data){
+        $scope.users = data.users;
+        $scope.currentPage = data.page;
+        $scope.pageSize = data.size;
+
+        var p = [];
+        for (var i = 0; i < data.totalPages; i++){
+          p.push(i);
+        }
+        $scope.pages = p;
+      }
+
+      UserService
+        .getPage($stateParams.page, $stateParams.size, $stateParams.query)
+        .success(function(data){
+          updatePage(data);
+        });
+
+      $scope.$watch('queryText', function(queryText){
+        UserService
+          .getPage($stateParams.page, $stateParams.size, queryText)
+          .success(function(data){
+            updatePage(data);
+          });
+      });
+
+      $scope.goToPage = function(page){
+        $state.go('app.admin.users', {
+          page: page,
+          size: $stateParams.size || 50
+        });
+      };
+
+      $scope.goUser = function($event, user){
+        $event.stopPropagation();
+
+        $state.go('app.admin.user', {
+          id: user._id
+        });
+      };
+
+      $scope.toggleCheckIn = function($event, user, index) {
+        $event.stopPropagation();
+
+        if (!user.status.checkedIn){
+          swal({
+            title: "Whoa, wait a minute!",
+            text: "You are about to check in " + user.profile.fname + " " + user.profile.lname + "!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, check them in.",
+            closeOnConfirm: false
+            },
+            function(){
+              UserService
+                .checkIn(user._id)
+                .success(function(user){
+                  $scope.users[index] = user;
+                  swal("Accepted", user.profile.fname + " " + user.profile.lname + ' has been checked in.', "success");
+                });
+            }
+          );
+        } else {
+          UserService
+            .checkOut(user._id)
+            .success(function(user){
+              $scope.users[index] = user;
+              swal("Accepted", user.profile.fname + " " + user.profile.lname + ' has been checked out.', "success");
+            });
+        }
+      };
+
+      $scope.acceptUser = function($event, user, index) {
+        $event.stopPropagation();
+
+        swal({
+          title: "Whoa, wait a minute!",
+          text: "You are about to accept " + user.profile.fname + " " + user.profile.lname + "!",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Yes, accept them.",
+          closeOnConfirm: false
+          }, function(){
+
+            swal({
+              title: "Are you sure?",
+              text: "Your account will be logged as having accepted this user. " +
+                "Remember, this power is a privilege.",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#DD6B55",
+              confirmButtonText: "Yes, accept this user.",
+              closeOnConfirm: false
+              }, function(){
+
+                UserService
+                  .admitUser(user._id)
+                  .success(function(user){
+                    $scope.users[index] = user;
+                    swal("Accepted", user.profile.fname + " " + user.profile.lname + ' has been admitted.', "success");
+                  });
+
+              });
+
+          });
+
+      };
+
+      $scope.toggleAdmin = function($event, user, index) {
+        $event.stopPropagation();
+
+        if (!user.admin){
+          swal({
+            title: "Whoa, wait a minute!",
+            text: "You are about make " + user.profile.fname + " " + user.profile.lname + " an admin!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, make them an admin.",
+            closeOnConfirm: false
+            },
+            function(){
+              UserService
+                .makeAdmin(user._id)
+                .success(function(user){
+                  $scope.users[index] = user;
+                  swal("Made", user.profile.fname + " " + user.profile.lname + ' an admin.', "success");
+                });
+            }
+          );
+        } else {
+          swal({
+            title: "Whoa, wait a minute!",
+            text: "You are about remove " + user.profile.fname + " " + user.profile.lname + " as admin!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, remove them as admin.",
+            closeOnConfirm: false
+            },
+            function(){
+              UserService
+                .removeAdmin(user._id)
+                .success(function(user){
+                  $scope.users[index] = user;
+                  swal("Removed", user.profile.fname + " " + user.profile.lname + ' an admin.', "success");
+                });
+            }
+          );
+        }
+      };
+
+      function formatTime(time){
+        if (time) {
+          return moment(time).format('MMMM Do YYYY, h:mm:ss a');
+        }
+      }
+
+      $scope.rowClass = function(user) {
+        if (user.admin){
+          return 'admin';
+        }
+        if (user.status.confirmed) {
+          return 'positive';
+        }
+        if (user.status.admitted && !user.status.confirmed) {
+          return 'warning';
+        }
+      };
+
+      function selectUser(user){
+        $scope.selectedUser = user;
+        $scope.selectedUser.sections = generateSections(user);
+        $('.long.user.modal')
+          .modal('show');
+      }
+
+      function generateSections(user){
+        return [
+          {
+            name: 'Basic Info',
+            fields: [
+              {
+                name: 'Created On',
+                value: formatTime(user.timestamp)
+              },{
+                name: 'Last Updated',
+                value: formatTime(user.lastUpdated)
+              },{
+                name: 'Confirm By',
+                value: formatTime(user.status.confirmBy) || 'N/A'
+              },{
+                name: 'Checked In',
+                value: formatTime(user.status.checkInTime) || 'N/A'
+              },{
+                name: 'Email',
+                value: user.email
+              },{
+                name: 'Team',
+                value: user.teamCode || 'None'
+              }
+            ]
+          },{
+            name: 'Profile',
+            fields: [
+              {
+                name: 'First Name',
+                value: user.profile.fname
+              },{
+                name: 'Last Name',
+                value: user.profile.lname
+              },{
+                name: 'School',
+                value: user.profile.school
+              },{
+                name: 'Grade',
+                value: user.profile.grade
+              },{
+                name: 'Major',
+                value: user.profile.major
+              },{
+                name: 'Gender',
+                value: user.profile.gender
+              },{ 
+                name: 'Ethnicity',
+                value: user.profile.ethnicity
+              },{   
+                name: 'Attended Hackathons',
+                value: user.profile.hacks
+              },{    
+                name: 'Attended AthenaHacks',
+                value: user.profile.attended
+              },{                    
+                name: 'Topic',
+                value: user.profile.topic
+              },{
+                name: 'Essay',
+                value: user.profile.essay
+              },{
+                name: 'Links',
+                value: user.profile.links
+              },{
+                name: 'Adult',
+                value: user.profile.adult
+              }
+
+            ]
+          },{
+            name: 'Confirmation',
+            fields: [
+              {
+                name: 'Phone Number',
+                value: user.confirmation.phoneNumber
+              },{
+                name: 'Vegetarian',
+                value: user.confirmation.vegetarian
+              },{
+                name: 'Vegan',
+                value: user.confirmation.vegan
+              },{
+                name: 'GlutenFree',
+                value: user.confirmation.glutenfree
+              },{
+                name: 'Shirt Size',
+                value: user.confirmation.shirtSize
+              },{
+                name: 'Resume',
+                value: user.confirmation.resume
+              },{
+                name: 'Travel',
+                value: user.confirmation.bus
+              },{
+                name: 'Emergency Contact',
+                value: user.confirmation.econtact
+              },{
+                name: 'Emergency Number',
+                value: user.confirmation.enumber,
+                type: 'boolean'
+              },{
+                name: 'Notes',
+                value: user.confirmation.notes
+              }
+            ]
+          }
+        ];
+      }
+
+      $scope.selectUser = selectUser;
+
+    }]);
+
+
---
+++ app/client/views/confirmation/confirmationCtrl.js
@@ -0,0 +1,82 @@
+angular.module('reg')
+  .controller('ConfirmationCtrl', [
+    '$scope',
+    '$rootScope',
+    '$state',
+    'currentUser',
+    'Utils',
+    'UserService',
+    function($scope, $rootScope, $state, currentUser, Utils, UserService){
+
+      // Set up the user
+      var user = currentUser.data;
+      $scope.user = user;
+      $scope.pastConfirmation = Date.now() > user.status.confirmBy;
+
+      $scope.formatTime = Utils.formatTime;
+
+      _setupForm();
+
+      $scope.fileName = user._id + "_" + user.profile.fname + "_" + user.profile.lname;
+
+      // -------------------------------
+
+      function _updateUser(e){
+        var confirmation = $scope.user.confirmation;
+
+        UserService
+          .updateConfirmation(user._id, confirmation)
+          .then(response => {
+            swal("Woo!", "You're confirmed!", "success").then(value => {
+              $state.go("app.dashboard");
+            });
+          }, response => {
+            swal("Uh oh!", "Something went wrong.", "error");
+          });
+      }
+
+      function _setupForm(){
+        // Semantic-UI form validation
+        $('.ui.form').form({
+          fields: {
+            shirt: {
+              identifier: 'shirt',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please give us a shirt size!'
+                }
+              ]
+            },
+            phone: {
+              identifier: 'phone',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter a phone number.'
+                }
+              ]
+            },
+            econtact: {
+              identifier: 'econtact',
+            },
+            enumber: {
+              identifier: 'enumber',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please enter a phone number.'
+                }
+              ]
+            },
+          }
+        });
+      }
+
+      $scope.submitForm = function(){
+        if ($('.ui.form').form('is valid')){
+          _updateUser();
+        }
+      };
+
+    }]);
