ccf1c912a533c3176329ee1ad08bf28c10e15dcc
Sophie Wu <23244275+kuanyiwu@users.noreply.github.com>
2019-01-03 20:04:49 -0800

    modify confirmation text and dashboard layout

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -5,7 +5,7 @@ angular.module('reg')
     .constant('DASHBOARD', {
         UNVERIFIED: 'You should have received an email asking you verify your email. Click the link in the email and you can start your application!',
         INCOMPLETE_TITLE: 'You still need to complete your application!',
-        INCOMPLETE: 'If you do not complete your application before the [APP_DEADLINE], you will not be considered for the admissions lottery!',
+        INCOMPLETE: 'If you do not complete your application before the [APP_DEADLINE], you will not be considered for the admissions!',
         SUBMITTED_TITLE: 'Your application has been submitted!',
         SUBMITTED: 'Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.\nPlease make sure your information is accurate before registration is closed!',
         CLOSED_AND_INCOMPLETE_TITLE: 'Unfortunately, registration has closed.',
@@ -14,10 +14,10 @@ angular.module('reg')
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
         CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
-        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to Athenahacks 2019! :(\nMaybe next year! We hope you see you again soon.',
+        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to Athenahacks 2019! :( \n Maybe next year! \n We hope you see you again soon.',
     })
     .constant('TEAM',{
-        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the lottery with a team.\nHowever, you can still form teams on your own before or during the event!',
+        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to register with a team.\nHowever, you can still form teams on your own before or during the event!',
     });
 
     
