55ae175f7f505ad562a58989aced47b71ff16d90
Sophie Wu <23244275+kuanyiwu@users.noreply.github.com>
2019-11-04 20:57:04 -0800

    background and update year

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -1,6 +1,6 @@
 angular.module('reg')
     .constant('EVENT_INFO', {
-        NAME: 'AthenaHacks 2019',
+        NAME: 'AthenaHacks 2020',
     })
     .constant('DASHBOARD', {
         UNVERIFIED: 'You should have received an email asking you verify your email. Click the link in the email and you can start your application!',
@@ -14,7 +14,7 @@ angular.module('reg')
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
         CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE]',
-        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to Athenahacks 2019! :( \n Maybe next year! \n We hope you see you again soon.',
+        DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to Athenahacks 2020! :( \n Maybe next year! \n We hope you see you again soon.',
     })
     .constant('TEAM',{
         NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to register with a team.\nHowever, you can still form teams on your own before or during the event!',
